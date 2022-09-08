d1e33d97b86374a91c7c1432cc819355fec4beea
andrew <andrew_mokryj@ukr.net>
2022-09-08 11:39:27 +0300

    Initial Commit

---
+++ test_widget/test_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_widgetApp.swift
+//  test_widget
+//
+//  Created by Andrew Mokryj on 08.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct test_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
