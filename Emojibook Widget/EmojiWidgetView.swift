90d8760e551d2a9778d25fbf5a5d719fe094938c
andrew <andrew_mokryj@ukr.net>
2022-09-09 11:34:35 +0300

    feature: add small, medium and large widgets

--- Emojibook Widget/EmojiWidgetView.swift
+++ Emojibook Widget/EmojiWidgetView.swift
@@ -9,6 +9,25 @@ import SwiftUI
 import WidgetKit
 
 struct EmojiWidgetView: View {
+    @Environment(\.widgetFamily) var family: WidgetFamily
+      let emojiDetails: EmojiDetails
+
+      @ViewBuilder
+      var body: some View {
+        switch family {
+        case .systemSmall:
+          SmallEmojiWidgetView(emojiDetails: emojiDetails)
+        case .systemMedium:
+          MediumEmojiWidgetView(emojiDetails: emojiDetails)
+        case .systemLarge:
+          LargeEmojiWidgetView(emojiDetails: emojiDetails)
+        @unknown default:
+          EmptyView()
+        }
+      }
+}
+
+struct SmallEmojiWidgetView: View {
   let emojiDetails: EmojiDetails
 
   var body: some View {
@@ -32,5 +51,51 @@ struct EmojiWidgetView_Previews: PreviewProvider {
   static var previews: some View {
     EmojiWidgetView(emojiDetails: EmojiProvider.random())
       .previewContext(WidgetPreviewContext(family: .systemSmall))
+  }
+}
+
+struct MediumEmojiWidgetView: View {
+
+  let emojiDetails: EmojiDetails
+
+  var body: some View {
+    ZStack {
+      Color(UIColor.systemIndigo)
+      HStack {
+        Text(emojiDetails.emoji)
+          .font(.system(size: 56))
+          .padding()
+        VStack(alignment: .leading) {
+          Text(emojiDetails.name)
+            .font(.headline)
+          Text(emojiDetails.description)
+        }
+        .foregroundColor(.white)
+        .padding([.top, .bottom, .trailing])
+      }
+    }
+  }
+}
+
+struct LargeEmojiWidgetView: View {
+  let emojiDetails: EmojiDetails
+
+  var body: some View {
+    ZStack {
+      Color(UIColor.systemIndigo)
+      VStack {
+        Text(emojiDetails.name)
+          .bold()
+          .font(.title)
+        Text(emojiDetails.emoji)
+          .font(.system(size: 60))
+          .padding()
+        Text(emojiDetails.description)
+          .font(.title3)
+      }
+      .foregroundColor(.white)
+      .multilineTextAlignment(.center)
+      .padding()
+    }
   }
 }
